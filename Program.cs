ebfe7b962bf81ae4f226e6c04ed76b0a3037fb4c
ChDavidsson <christiandavidsson@hotmail.com>
2026-09-25 00:26:35 +0200

    save

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FunNameGenerator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Du ska skapa ett program som gör ett “random—fun-namegeneration” åt användaren.
+        Console.WriteLine("/// WELCOME TO FUN NAME GENERATOR ///");
+        // Instruktioner:
+        // Be användaren skriva in sitt förnamn.
+        Console.WriteLine("Please provide your name:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is: {name}");
+        // Be användaren skriva in sin favoritfärg.
+
+        Console.WriteLine("Please provide your favorite color:");
+        string color = Console.ReadLine();
+
+        Console.WriteLine($"Your favorite color is: {color}");
+        // Sätt ihop dessa två till ett roligt namn.
+        // Exempel: “Anna” + “Blå” → Lil Anna of Blå Angeles
+
+        string funName = ($"")
+        // Skriv ut resultatet på skärmen.
+        // Tips:
+        // Använd Console.ReadLine() för att ta in text.
+        // Använd Console.WriteLine() för att skriva ut text.
+        // Du kan använda + för att slå ihop strängar
+    }
+}
